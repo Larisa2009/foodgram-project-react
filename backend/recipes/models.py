6e02b3a4b187f21cc78271149ccfacbb992dee99
blabla <blabla@gmail.com>
2023-09-17 14:40:57 +0300

    17.09

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,6 @@
 from colorfield.fields import ColorField
 from django.contrib.auth import get_user_model
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from recipes.validators import validate_slug
